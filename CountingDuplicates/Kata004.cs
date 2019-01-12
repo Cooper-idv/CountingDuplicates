61fcfe06c5a3367038b11b89d5998a917089eee2
Cooper Chen <cooper.chen@titansoft.com.sg>
2019-01-12 16:39:48 +0700

    Count the number of Duplicates Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
    Example
    "abcde" -> 0 # no characters repeats more than once
    "aabbcde" -> 2 # 'a' and 'b'
    "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
    "indivisibility" -> 1 # 'i' occurs six times
    "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
    "aA11" -> 2 # 'a' and '1'
    "ABBA" -> 2 # 'A' and 'B' each occur twice

---
+++ CountingDuplicates/Kata004.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CountingDuplicates
+{
+    public class Kata004
+    {
+        public static int DuplicateCount(string str)
+        {
+            int charDupCount = 0;
+            if (string.IsNullOrEmpty(str))
+            {
+                return charDupCount;
+            }
+            var dict = new Dictionary<char, int>();
+            foreach (char c in str.ToLower())
+            {
+                if (dict.ContainsKey(c))
+                {
+                    dict[c] = dict[c]+1;
+                }
+                else
+                {
+                    dict.Add(c,1);
+                }
+            }
+
+            foreach (var item in dict)
+            {
+                if (item.Value > 1)
+                    charDupCount++;
+            }
+
+            return charDupCount;
+        }
+    }
+}
---
+++ CountingDuplicatesTests/Kata004Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CountingDuplicates;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CountingDuplicates.Tests
+{
+    [TestClass()]
+    public class Kata004Tests
+    {
+        [TestMethod()]
+        public void DuplicateCountTest()
+        {
+            Assert.AreEqual(0, Kata004.DuplicateCount(""));
+            Assert.AreEqual(0, Kata004.DuplicateCount("abcde"));
+            Assert.AreEqual(2, Kata004.DuplicateCount("aabbcde"));
+            Assert.AreEqual(2, Kata004.DuplicateCount("aabBcde"), "should ignore case");
+            Assert.AreEqual(1, Kata004.DuplicateCount("Indivisibility"));
+            Assert.AreEqual(2, Kata004.DuplicateCount("Indivisibilities"), "characters may not be adjacent");
+        }
+    }
+}
